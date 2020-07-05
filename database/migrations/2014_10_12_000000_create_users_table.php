89311b77e7a35356d83d466d73d3840f832d2fea
czycc <602155776@qq.com>
2020-07-05 21:39:15 +0800

    初始化

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('job_num')->comment('工号');
+            $table->string('unit')->comment('单位信息');
+            $table->string('department')->comment('所属部门');
+            $table->string('job')->comment('职务');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('avatar')->nullable()->comment('头像');
+            $table->string('wx_avatar')->nullable()->comment('头像');
+
+//            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
