6f8ffe1925b4a622ebe0c878b89aa81f827b2c03
zhaoyang <602155776@qq.com>
2020-07-19 22:39:46 +0800

    核销

--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -12,7 +12,7 @@ class VerificationCodeRequest extends FormRequest
                 'required',
                 'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
             ],
-            'category' => 'required|in:login,out_task,visit_task,new_task,invite_task,draw'
+            'category' => 'required|in:login,out_task,visit_task,new_task,invite_task,draw,verify'
         ];
     }
 
