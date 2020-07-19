10949a75cb7b3ff040ae30108e30a75bc9da5afe
zhaoyang <602155776@qq.com>
2020-07-19 19:33:09 +0800

    抽奖回传

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
     //抽奖细节
     Route::get('draw/{draw}', 'DrawController@show');
     Route::get('draw_item/{draw}', 'DrawController@drawItem');
-
+    Route::post('draw/store', 'DrawController@store');
     //首页数据
     Route::get('home', 'CommonController@home');
 
