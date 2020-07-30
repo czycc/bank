ccd0eaaa52dcb64c3791df507b0519e4e62f9849
czycc <602155776@qq.com>
2020-07-30 14:08:14 +0800

    细节优化

--- app/Http/Controllers/Api/OutTaskController.php
+++ app/Http/Controllers/Api/OutTaskController.php
@@ -61,9 +61,9 @@ class OutTaskController extends Controller
 
         if (
             !OutTask::where('id', $request->out_task_id)
-            ->where('enable', 1)
-            ->where('start', '<', Carbon::now())
-            ->where('end', '>', Carbon::now())
+//            ->where('enable', 1)
+//            ->where('start', '<', Carbon::now())
+//            ->where('end', '>', Carbon::now())
             ->first()
         ) {
             abort(400, '很抱歉，活动已经结束');
