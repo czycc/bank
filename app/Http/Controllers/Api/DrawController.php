e2e2b7867b0ecc49754ba7d44e9972aeaa3daa81
zhaoyang <602155776@qq.com>
2020-10-13 23:53:31 +0800

    光大银行修改短信发送

--- app/Http/Controllers/Api/DrawController.php
+++ app/Http/Controllers/Api/DrawController.php
@@ -9,6 +9,7 @@ use App\Models\DrawItem;
 use App\Models\DrawItemUser;
 use App\Models\User;
 use Carbon\Carbon;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
@@ -70,6 +71,21 @@ class DrawController extends Controller
             'user_id' => $data['user_id'],
             'draw_item_id' => $draw_item_id,
             'draw_id' => $draw->id
+        ]);
+        $draw_item = DrawItem::find($draw_item_id);
+
+        //发送中奖信息
+        $client = new Client([
+            'timeout' => 10.0,
+            'base_uri' => 'http://112.81.84.7:8000'
+        ]);
+        $client->request('POST', 'api/v1/common/sms/send', [
+            'json' => [
+                'msg' => '000071000220200919000000014169'.
+                    str_pad($draw_item->reward, 20)
+                    . $data['phone'] .'         1',
+                'category' => 'normal'
+            ]
         ]);
 
         unset($a);
